ed12b9e44dd156444d27cb85ee5fc800e8e2e5ff
yousseffahmy220-ux <youssef.fahmy220@gmail.com>
2026-06-04 20:34:29 +0200

    fix: remove duration from scenario config

--- src/Scenarios.py
+++ src/Scenarios.py
@@ -6,7 +6,7 @@ from . import Constants
 def create_earth_moon():
     earth = Body("Earth",Constants.EARTH_MASS,Constants.EARTH_RADIUS,[0,0,0],[0,0,0])
     moon = Body("Moon",Constants.MOON_MASS,Constants.MOON_RADIUS,[Constants.MOON_START_X,0,0],[0,Constants.MOON_CIRCULAR_VELOCITY,0])
-    config = {"time_step": Constants.DEFAULT_TIME_STEP, "duration": Constants.MOON_ORBITAL_PERIOD}
+    config = {"time_step": Constants.DEFAULT_TIME_STEP}
     return [earth,moon],config
 
 def create_cannon_shot(speed,angle_deg):
@@ -18,5 +18,5 @@ def create_cannon_shot(speed,angle_deg):
     vel_x = speed * math.cos(angle_rad)
     vel_y = speed * math.sin(angle_rad)
     projectile = Body("Projectile",Constants.PROJECTILE_MASS,Constants.PROJECTILE_RADIUS,[pos_x,pos_y,0],[vel_x,vel_y,0])
-    config = {"time_step": Constants.SECOND,"duration": 7 * Constants.DAY}
+    config = {"time_step": Constants.SECOND}
     return [earth,moon,projectile],config
