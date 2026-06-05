54b33568ae549024b5029da4df59939bf5fbd3a0
yousseffahmy220-ux <youssef.fahmy220@gmail.com>
2026-06-05 19:38:29 +0200

    fix: add sys.path setup to Test_Integrator imports

--- Tests/Test_Integrator.py
+++ Tests/Test_Integrator.py
@@ -1,5 +1,8 @@
 import unittest
 import numpy as np
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../src"))
 import Integrator
 from Body import Body
 from Constants import G
