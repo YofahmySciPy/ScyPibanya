7a3e7903c75855fedef46041eb62a035b298775b
Bastian Klumpp <bastianklumpp11@gmail.com>
2026-06-08 19:56:51 +0200

    refactor: redundant comments

--- src/Collisions.py
+++ src/Collisions.py
@@ -15,7 +15,6 @@ class Collisions:
                 distance = np.linalg.norm(b1.position - b2.position)
 
                 if distance <= b1.radius + b2.radius:
-                    # build new merged body
                     new_body = Collisions.merge(b1, b2, dt)
 
                     # remove the two old bodies, add the new one,
@@ -25,25 +24,19 @@ class Collisions:
                     new_list.append(new_body)
                     return Collisions.handle(new_list, dt)
 
-        # returns the list if no collisions found
         return bodies
 
     @staticmethod
     def merge(b1, b2, dt):
 
-        # new name
         name_new = b1.name + "_" + b2.name
 
-        # new mass
         m_new = b1.mass + b2.mass
 
-        # new velocity
         v_new = (b1.mass * b1.velocity + b2.mass * b2.velocity) / m_new
 
-        # new position at center of mass
         r_new = (b1.mass * b1.position + b2.mass * b2.position) / m_new
 
-        # new radius at same density
         radius_new = (b1.radius**3 + b2.radius**3) ** (1 / 3)
 
         new_body = Body(
