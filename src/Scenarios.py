827b9de45558127bd8c8371ad73bd591de2cea66
Bastian Klumpp <bastianklumpp11@gmail.com>
2026-06-25 21:58:05 +0200

    feature: added the 67-easter-egg

--- src/Scenarios.py
+++ src/Scenarios.py
@@ -21,3 +21,9 @@ def create_cannon_shot(speed,angle_deg):
     projectile = Body("Projectile",Constants.PROJECTILE_MASS,Constants.PROJECTILE_RADIUS,[pos_x,pos_y,0],[vel_x,vel_y,0])
     config = {"time_step": Constants.DEFAULT_CANNON_TIME_STEP}
     return [earth,moon,projectile],config
+
+def create_67():
+    earth = Body("Earth",Constants.EARTH_MASS,Constants.EARTH_RADIUS,[0,0,0],[0,0,0])
+    moon = Body("Moon",Constants.MOON_MASS,Constants.MOON_RADIUS,[Constants.MOON_START_X,0,0],[0,Constants.MOON_CIRCULAR_VELOCITY / 2,0])
+    config = {"time_step": Constants.DEFAULT_EARTH_MOON_TIME_STEP}
+    return [earth,moon],config
